0d5ad9ca21dd009626036779f5af90ad443e639b
Alexander Fengler <alexanderfengler@gmx.de>
2023-09-07 15:12:27 -0400

    delete some unnecessary code/comments

--- setup.py
+++ setup.py
@@ -41,43 +41,8 @@ except ImportError:
                   include_dirs=[np.get_include()]),
     ]
 
-# setup(
-#     name="hddm-wfpt",
-#     version="0.1.1",
-#     author="Thomas V. Wiecki, Imri Sofer, Michael J. Frank, Mads Lund Pedersen, Alexander Fengler, Lakshmi Govindarajan, Krishn Bera",
-#     author_email="thomas.wiecki@gmail.com",
-#     url="http://github.com/hddm-devs/hddm",
-#     packages=["hddm_wfpt"],  # 'hddm.cnn', 'hddm.cnn_models', 'hddm.keras_models',
-#     description="HDDM is a python module that implements Hierarchical Bayesian estimation of Drift Diffusion Models.",
-#     setup_requires=["numpy >= 1.23.4", "cython >= 0.29.32"],
-#     install_requires=["numpy >=1.23.4", "scipy >= 1.9.1", "cython >= 0.29.32"],
-#     include_dirs=[np.get_include()],
-#     classifiers=[
-#         "Development Status :: 5 - Production/Stable",
-#         "Environment :: Console",
-#         "Operating System :: OS Independent",
-#         "Intended Audience :: Science/Research",
-#         "License :: OSI Approved :: BSD License",
-#         "Programming Language :: Python",
-#         "Topic :: Scientific/Engineering",
-#     ],
-#     ext_modules=ext_modules,
-# )
-
-            # {name="Imri Sofer", email=""},
-            # {name="Krishn Bera", email=""},
-            # {name="Paul Xu", email=""},
-            # {name="Thomas V. Wiecki", email=""},
-            # {name="Michael J. Frank", email=""},
-
 setup(
     packages=["hddm_wfpt"],
-    #setup_requires=["numpy >= 1.23.4", "cython >= 0.29.32"],
-    #install_requires=["numpy >=1.23.4", "scipy >= 1.9.1", "cython >= 0.29.32"],
-    #dependencies = ["numpy >=1.23.4", "scipy >= 1.9.1", "cython >= 0.29.32"],
     include_dirs=[np.get_include()],
     ext_modules=ext_modules,
 )
-
-
-#  packages=['hddm', 'hddm.tests', 'hddm.models', 'hddm.examples', 'hddm.torch', 'hddm.torch_models', 'hddm.simulators'],
